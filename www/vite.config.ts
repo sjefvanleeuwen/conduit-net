a95e5500e9bee704e064b1decfde030e7292bb9b
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2025-11-22 19:28:22 +0100

    resolve rollup

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: './',
   base: './',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        blog: resolve(__dirname, 'blog.html'),
+      },
+    },
   },
   server: {
     port: 3000
