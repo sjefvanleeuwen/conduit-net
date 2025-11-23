814cfb77421bb04f80664d1fbdf9e605488f6fab
Sjef van Leeuwen <36629650+sjefvanleeuwen@users.noreply.github.com>
2025-11-23 11:08:13 +0100

    use webfooter component across all pages

--- www/src/stats.ts
+++ www/src/stats.ts
@@ -1,5 +1,6 @@
 import './styles/main.scss';
 import './components/conduit-nav/conduit-nav';
+import './components/conduit-footer/conduit-footer';
 import './components/conduit-hero';
 
 console.log('ConduitNet Stats Page Loaded');
